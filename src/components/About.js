f1089774e0f03970106eee2790c9d676a08bc8d9
albertg1114 <ajgonzalez1114@gmail.com>
2018-12-12 12:13:30 -0500

    add flexbox to about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,22 @@ const About = (props) => {
         <div className='About'>
             <div className='About-Content-Wrapper'>
             <div className='About-Row-Top'>
-            
+            <div className='Picture-Container'>
+                <img id='profilePicture' src="/" alt="" />
+                </div>
+            <div id='aboutMe'>
+                    I'm a web developer based in Orlando, Fl with a passion for solving problems, an eye for
+                    design, and love of all things tech. Lets build something!
+            </div>
             </div>
             <div className='About-Row-Bottom'>
-            
+            <div id='aboutMe2'>
+                    I'm a web developer based in Orlando, Fl with a passion for solving problems, an eye for
+                    design, and love of all things tech. Lets build something!
+            </div>
+            <div className='Picture-Container'>
+                <img id='profilePicture2' src="/" alt="" />
+                </div>
             </div>
             </div>
         </div>
