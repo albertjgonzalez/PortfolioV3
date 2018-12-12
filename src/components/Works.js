e656512fd84f3dbdacaf5b319fd4b0b9dc93d028
albertg1114 <ajgonzalez1114@gmail.com>
2018-12-12 12:41:46 -0500

    added flexbox to works section

--- src/components/Works.js
+++ src/components/Works.js
@@ -8,13 +8,31 @@ class Works extends Component {
         <div className='Works'>
 <div className='Work-Content-Wrapper'>
             <div className='Work-Row-Top'>
-            
+            <div className='Picture-Container'>
+                <img id='Work1-Img' src="/" alt="" />
+                </div>
+            <div className="Work-Description">
+                    I'm a web developer based in Orlando, Fl with a passion for solving problems, an eye for
+                    design, and love of all things tech. Lets build something!
+            </div>
             </div>
             <div className='Work-Row-Center'>
-            
+            <div className="Work-Description">
+                    I'm a web developer based in Orlando, Fl with a passion for solving problems, an eye for
+                    design, and love of all things tech. Lets build something!
+            </div>
+            <div className='Picture-Container'>
+                <img id='Work2-Img' src="/" alt="" />
+                </div>
             </div>
             <div className='Work-Row-Bottom'>
-            
+            <div className='Picture-Container'>
+                <img id='Work3-Img' src="/" alt="" />
+                </div>
+            <div className="Work-Description">
+                    I'm a web developer based in Orlando, Fl with a passion for solving problems, an eye for
+                    design, and love of all things tech. Lets build something!
+            </div>
             </div>
             </div>
         </div>
